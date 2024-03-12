f654dae5b5de558e2cd77811f73e2611b1f94a82
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2024-03-11 21:54:28 -0400

    On branch develop 	modified:   main.m

--- SMS-EPI/examples/1/main.m
+++ SMS-EPI/examples/1/main.m
@@ -7,8 +7,21 @@ get_code_and_set_paths;
 % set file names and path
 set_experimental_parameters_ge;
 
-% remove existing local .h5 and .mat files
-% rm ghostcal*.h5 mb1*.h5 rest*.h5 task*.h5 *.mat
+% load ghost correction parameters
+load a
+
+% load 2d calibration data for slice GRAPPA
+load dcal
+
+% reconstruct
+fn = 'rest.h5';   % custom data file format for raw data storage
+nFrames = 10;  %  number of frames to reconstruct (out of 392)
+recon_timeseries;
+
+return
+
+% The following code generates the files 'a.mat' and 'dcal.mat',
+% and write the raw data to a custom .h5 file format
 
 % EPI ghost calibration. Saves linear ghost correction parameters in a.mat.
 % If you observe phase wraps in plots, change 'del' in set_experimental_params.m.
@@ -19,16 +32,14 @@ get_ghost_calibration_data;
 save a a
 
 % Get slice-GRAPPA calibration data (dcal).
-% Load data from ScanArchive file, or load 'dcal' array already created for you
+% Load data from ScanArchive file
 D = readraw(datafile_mb1, scanner);
 hmriutils.epi.io.draw2hdf(D, etl, np*mb, 'mb1.h5');
 get_acs_data;
 save('dcal.mat', 'dcal', '-v7.3');
 
-% Load and reconstruct a few frames from the resting-state run
+% Load the resting-state run
 D = readraw(datafile_rest, scanner);
 fn = 'rest.h5';   % 'fn' is used by recon_timeseries.m as well
 hmriutils.epi.io.draw2hdf(D, etl, np, fn, 'maxFramesPerFile', 50);
-nFrames = 10;  %  number of frames to reconstruct (out of 392)
-recon_timeseries;
 
